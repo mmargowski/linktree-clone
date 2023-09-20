363adbe4f237609448ffe10bdf72ecf5dab6e588
Marius Margowski <mariusmargowski89@gmail.com>
2023-09-20 22:39:35 +0200

    Added landingpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<section class="p-8 min-h-screen">
+		<div class="container mx-auto text-center">
+			<h1 class="text-4xl font-extrabold mb-4">Welcome to Linktree Clone</h1>
+			<p class="text-lg mb-8">Organize your links and share them with ease!</p>
+			<a href="/login" class="btn btn-primary rounded-full text-lg">Create your account!</a>
+		</div>
+	</section>
+</main>
