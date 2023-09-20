bec4822da13b3437d684bbec12c134eae0347243
Marius Margowski <mariusmargowski89@gmail.com>
2023-09-20 22:44:07 +0200

    Show login button only if user is not logged in.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
+<script lang="ts">
+	import { auth, userData } from '$lib/firebase';
+	import { userStore } from 'sveltefire';
+
+	const user = userStore(auth);
+
+	$: href = `/${$userData?.username}/edit`;
+</script>
+
 <main>
 	<section class="p-8 min-h-screen">
 		<div class="container mx-auto text-center">
 			<h1 class="text-4xl font-extrabold mb-4">Welcome to Linktree Clone</h1>
 			<p class="text-lg mb-8">Organize your links and share them with ease!</p>
-			<a href="/login" class="btn btn-primary rounded-full text-lg">Create your account!</a>
+			{#if !$user}
+				<a href="/login" class="btn btn-primary rounded-full text-lg">Create your account!</a>
+			{:else}
+				<a {href} class="btn btn-primary rounded-full text-lg">Take me to my account!</a>
+			{/if}
 		</div>
 	</section>
 </main>
