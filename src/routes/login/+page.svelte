2d9670f21fc945c10acb87233cc9414f7aa5f1b9
Marius Margowski <mariusmargowski89@gmail.com>
2023-09-21 16:21:07 +0200

    Added navigation to login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -31,7 +31,10 @@
 {#if $user}
 	<h2 class="card-title">Welcome {$user.displayName}</h2>
 	<p class="text-center text-success">You are logged in!</p>
-	<button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
+	<div>
+		<button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
+		<a href="/login/username" class="btn btn-primary"> Continue </a>
+	</div>
 {:else}
 	<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
